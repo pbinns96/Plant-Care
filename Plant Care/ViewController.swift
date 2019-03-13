59f3de52528d476c8b44450813cd84ddbd917b11
Payton Binns <payton@paytons-macbook-pro.local>
2019-03-13 13:31:49 -0500

    added plant structure outline

--- Plant Care/ViewController.swift
+++ Plant Care/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    struct Plant {
+        var isDead = Bool(false)
+        //all 3 areas need to be at low/high end of range or 1 area needs to be over/under max/min
+        var isDehydrated = Bool(false)
+        var isOverwatered = Bool(false)
+        var isTooHot = Bool(false)
+        var isTooCold = Bool(false)
+        var tooMuchSun = Bool(false)
+        var notEnoughSun = Bool(false)
+        var baseTemperature = Int(0)
+        //min temp: max temp: acceptable temp range:
+        var baseSunExposure = Int(0)
+        //min sun: max sun: acceeptable sun range:
+        var baseHydration = Int(0)
+        //min hydration: max hydration: acceptable hydration range:
+       
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
