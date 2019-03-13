20e6050eedacd950c18aa75773f7003d4ab645fb
Payton Binns <payton@paytons-macbook-pro.local>
2019-03-13 13:10:39 -0500

    Initial Commit

---
+++ Plant Care/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plant Care
+//
+//  Created by Payton Binns on 3/13/19.
+//  Copyright © 2019 Payton Binns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
