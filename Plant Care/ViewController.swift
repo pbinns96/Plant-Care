643ffce808eeffd4b440bc2971b5a564ce1f634e
Payton Binns <payton@paytons-macbook-pro.local>
2019-03-28 13:07:58 -0500

    added sound, safari services, alerts. needs help with segue

--- Plant Care/ViewController.swift
+++ Plant Care/ViewController.swift
@@ -7,58 +7,156 @@
 //
 
 import UIKit
-
+import AVFoundation
+import SafariServices
 class ViewController: UIViewController {
 
     struct Plant {
-        var isDead = Bool(false)
-        //all 3 areas need to be at low/high end of range or 1 area needs to be over/under max/min
-        var isDehydrated = Bool(false)
-        var isOverwatered = Bool(false)
-        var isTooHot = Bool(false)
-        var isTooCold = Bool(false)
-        var tooMuchSun = Bool(false)
-        var notEnoughSun = Bool(false)
-        var baseTemperature = Int(0)
-        //min temp: max temp: acceptable temp range:
-        var baseSunExposure = Int(0)
-        //min sun: max sun: acceeptable sun range:
-        var baseHydration = Int(0)
-        //min hydration: max hydration: acceptable hydration range:
+        
+        var baseTemperature = Int(50)
+        //min temp:45 max temp: 80 acceptable temp range: 55-70
+        var baseSunExposure = Int(50)
+        //min sun: 30 max sun: 95 acceeptable sun range: 70-85
+        var baseHydration = Int(50)
+        //min hydration: <40 max hydration: >90 acceptable hydration range:60-80
        }
-    
+    var plantHealth = Plant ()
+    var audioPlayer = AVAudioPlayer()
+    @IBOutlet weak var heatButton: UIButton!
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var shadeButton: UIButton!
+    @IBOutlet weak var sunButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let sound = Bundle.main.path(forResource: "waterSound", ofType: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        }
+        catch {
+            print("error")
+        }
     }
 
     @IBAction func waterButtonPressed(_ sender: Any) {
+        plantHealth.baseHydration += 5
+        print (plantHealth.baseHydration)
+        heatButton.alpha = 0.0
+        acButton.alpha = 0.0
+        shadeButton.alpha = 0.0
+        sunButton.alpha = 0.0
+        dangerNotification ()
+        plantDeath ()
+        audioPlayer.play()
     }
     @IBAction func temperatureButtonPressed(_ sender: Any) {
+        heatButton.alpha = 1.0
+        acButton.alpha = 1.0
+       
     }
     @IBAction func heatButtonPressed(_ sender: Any) {
+        plantHealth.baseTemperature += 5
+        heatButton.alpha = 0.0
+        acButton.alpha = 0.0
+        shadeButton.alpha = 0.0
+        sunButton.alpha = 0.0
+        dangerNotification()
+        plantDeath()
+         print (plantHealth.baseTemperature)
     }
     @IBAction func acButtonPressed(_ sender: Any) {
+        plantHealth.baseTemperature -= 5
+        heatButton.alpha = 0.0
+        acButton.alpha = 0.0
+        shadeButton.alpha = 0.0
+        sunButton.alpha = 0.0
+        dangerNotification()
+        plantDeath()
     }
     @IBAction func sunlightButtonPressed(_ sender: Any) {
+        shadeButton.alpha = 1.0
+        sunButton.alpha = 1.0
+
     }
     @IBAction func shadeButtonPressed(_ sender: Any) {
+        plantHealth.baseSunExposure -= 5
+        plantHealth.baseTemperature -= 2
+        heatButton.alpha = 0.0
+        acButton.alpha = 0.0
+        shadeButton.alpha = 0.0
+        sunButton.alpha = 0.0
+        dangerNotification()
+        plantDeath()
     }
     @IBAction func sunButtonPressed(_ sender: Any) {
+        plantHealth.baseSunExposure += 5
+        plantHealth.baseTemperature += 2
+        heatButton.alpha = 0.0
+        acButton.alpha = 0.0
+        shadeButton.alpha = 0.0
+        sunButton.alpha = 0.0
+        dangerNotification()
+        plantDeath()
     }
-    /*let alert = UIAlertController (title:"ALERT" , message: "Hey \(theirName!)", preferredStyle: UIAlertController.Style.alert)
-    alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
-    present (alert, animated: true, completion: nil)*/
+    func alert () {
+        let alertDanger = UIAlertController (title:"ALERT" , message: "This action may be harmful to the plant.", preferredStyle: UIAlertController.Style.alert)
+        alertDanger.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
+        present (alertDanger, animated: true, completion: nil)}
+    func dangerNotification () {
+    if plantHealth.baseHydration >= 80 {
+            alert ()
+        }
+        if plantHealth.baseHydration <= 45 {
+            alert()
+        }
+        if plantHealth.baseSunExposure >= 85 {
+            alert ()
+        }
+        if plantHealth.baseSunExposure <= 45 {
+            alert()
+        }
+        if plantHealth.baseTemperature >= 75 {
+            alert ()
+        }
+        if plantHealth.baseTemperature <= 48 {
+            alert()
+        }
+    }
+    func plantDeath () {
+        if plantHealth.baseHydration >= 90 {
+            performSegue(withIdentifier: "gameOverSegue", sender: nil)
+        }
+        if plantHealth.baseHydration <= 40 {
+           performSegue(withIdentifier: "gameOverSegue", sender: nil)
+        }
+        if plantHealth.baseSunExposure >= 95 {
+            performSegue(withIdentifier: "gameOverSegue", sender: nil)
+        }
+        if plantHealth.baseSunExposure <= 30 {
+           performSegue(withIdentifier: "gameOverSegue", sender: nil)
+        }
+        if plantHealth.baseTemperature >= 80 {
+            performSegue(withIdentifier: "gameOverSegue", sender: nil)
+        }
+        if plantHealth.baseTemperature <= 45 {
+            performSegue(withIdentifier: "gameOverSegue", sender: nil)
+        }
+        
+    }
+var urlLink = "https://www.ftd.com/blog/share/orchid-care"
     
-    /*var urlIntermediary = "http://www.google.com/search?q="
-    
-    @IBAction func searchButtonPressed(_ sender: Any) {
-        urlIntermediary += String(searchTermTextField.text ?? "search")
-        let myUrl = URL (string: urlIntermediary)
+    @IBAction func learnMorePressed(_ sender: Any) {
+        let myUrl = URL (string: urlLink)
         let searchView = SFSafariViewController(url: myUrl!)
-        present(searchView, animated: true)*/
+        present(searchView, animated: true)
+    }
 }
 class ViewControllerTwo: ViewController {
+    @IBAction func startOverPressed(_ sender: Any) {
+         plantHealth.baseTemperature = 50
+         plantHealth.baseSunExposure = 50
+         plantHealth.baseHydration = 50
+    }
     
 }
 
